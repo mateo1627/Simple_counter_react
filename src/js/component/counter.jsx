92f48fed2488914a4126ef7a026a2747be67b00c
mateo1627 <140858704+mateo1627@users.noreply.github.com>
2024-07-09 15:13:13 +0000

    Improvements

--- src/js/component/counter.jsx
+++ src/js/component/counter.jsx
@@ -9,10 +9,13 @@ let Counter = ({second, second2, second3, second4, second5, second6}) => {
   <div className="card-body d-flex col-12 justify-content-center">
 
     <Digit digit={"🕟"}/>
+
     <Digit digit={second6}/>
     <Digit digit={second5}/>
+    <Digit digit=":"/>
     <Digit digit={second4}/>
     <Digit digit={second3}/>
+    <Digit digit=":"/>
     <Digit digit={second2}/>
     <Digit digit={second}/>
     
--- src/js/index.js
+++ src/js/index.js
@@ -10,35 +10,34 @@ import Home from "./component/home.jsx";
 
 
 //render your react application
-let second = 0;
-let second2 = 0;
-let second3 = 0;
-let second4 = 0;
-let second5 = 0;
-let second6 = 0;
+let second, second2, second3, second4, second5, second6;
+second = second2 = second3 = second4 = second5 =second6 = 0;
+
 setInterval(function(){
     second++;
-    if(second === 60){
+    if(second === 9){
         second = 0;
         second2++;
     }
-    if(second2 ===60){
-        second2=0;
+    if(second2 ===6){
+        second= second2 = 0 ;
         second3++
     }
-    if(second3 ===24){
-        second3=0;
+    if(second3 ===9){
+        second = second2= second3 = 0;
         second4++
     }
-    if(second4 ===365){
-        second4=0;
+    if(second4 ===6){
+        second = second2= second3=second4 = 0;
         second5++
     }
-    if(second5 ===1){
-        second5=0;
+    if(second5 ===4){
+        second = second2 = second3 = second4 = second5 = 0;
         second6++
     }
-    ReactDOM.createRoot(document.getElementById('app')).render(<Home second={second} second2={second2} second3={second3} second4={second4} second5={second5} second6={second6}/>)},1000);
-
-
+    if(second6 ===2 && second5 === 4){
+        second = second2 = second3 = second4 = second5 =second6 = 0;
+    }
+    
+    ReactDOM.createRoot(document.getElementById('app')).render(<Home second={second} second2={second2} second3={second3} second4={second4} second5={second5} second6={second6}/>)},0.000001);
     
